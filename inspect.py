185b5aee27f428b7a51adc9535c456c5b14749c6
arturgontijo <arturgontijo@gmail.com>
2019-06-12 09:52:58 -0300

    Adding Keras.

--- inspect.py
+++ inspect.py
@@ -3,13 +3,17 @@ import caffe
 import cntk
 
 import keras
+from keras.models import model_from_json, load_model
 
 import tensorflow as tf
 from tensorflow.python import pywrap_tensorflow
 
 
-def inspect_caffe(model_file, prototxt_file):
-    net = caffe.Net(prototxt_file, model_file, caffe.TEST)
+def inspect_caffe(model_file="", prototxt_file=""):
+    if model_file:
+        net = caffe.Net(prototxt_file, model_file, caffe.TEST)
+    else:
+        net = caffe.Net(prototxt_file, caffe.TEST)
     print("net.inputs:", net.inputs)
     print("net.inputs(shape:", net.blobs[net.inputs[0]].data.shape)
     print("net.params:", net.params)
@@ -21,9 +25,16 @@ def inspect_cntk(model_file):
     print(m)
 
 
-def inspect_keras(model_file):
-    m = keras.models.load_model(model_file)
-    m.summary()
+def inspect_keras(model_file, weights_file=""):
+    if weights_file:
+        with open(model_file, 'r') as f:
+            m = model_from_json(f.read())
+        m.load_weights(weights_file)
+    else:
+        m = keras.models.load_model(model_file)
+        m.summary()
+    print("Input: ", m.input)
+    print("Shape: ", m.input.shape)
 
 
 def inspect_tensorflow(model_folder):
@@ -40,8 +51,9 @@ def main():
         inspect_tensorflow(model_file)
 
     elif opt == "keras":
-        model_file = input("[{}] Model (Checkpoint Dir): ".format(opt))
-        inspect_keras(model_file)
+        model_file = input("[{}] Model (Architecture JSON): ".format(opt))
+        weights_file = input("[{}] Weights (.h5): ".format(opt))
+        inspect_keras(model_file, weights_file)
 
     elif opt == "cntk":
         model_file = input("[{}] Model File: ".format(opt))
